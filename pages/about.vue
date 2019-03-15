35c287d4c70bbc5a72526788b3a785f727292502
Very Dev 01 <giideonsiregar@gmail.com>
2019-03-15 17:06:49 +0700

    install scrolleble container

--- pages/about.vue
+++ pages/about.vue
@@ -34,6 +34,16 @@
                         Our objective is to drive and develop the energy efficiency market in Asia Pacific. In a region experiencing soaring energy prices due to drastic cuts to energy subsidies and increasing energy demand, we strive to create solutions for our clients that help them remain competitive.
                         We improve existing buildings by performing highly detailed Energy Audits that provide effective savings opportunities. Synergy Efficiency Solutions assists companies in the implementation of savings solutions by minimizing investment costs through our ESCO financing options.
                     </p>
+                    <p class="story">Synergy Efficiency Solutions is a company founded on the principles of integrity and loyalty. We pride ourselves in our world-class energy solutions and the success we achieve for our clients.
+                        By combining our expert energy engineers with the latest technologies and concepts for building optimization, our mission is to build the energy efficiency market through the continuous improvement of existing facilities and the creation of efficient, sustainable, new buildings.
+                        Our objective is to drive and develop the energy efficiency market in Asia Pacific. In a region experiencing soaring energy prices due to drastic cuts to energy subsidies and increasing energy demand, we strive to create solutions for our clients that help them remain competitive.
+                        We improve existing buildings by performing highly detailed Energy Audits that provide effective savings opportunities. Synergy Efficiency Solutions assists companies in the implementation of savings solutions by minimizing investment costs through our ESCO financing options.
+                    </p>
+                    <p class="story">Synergy Efficiency Solutions is a company founded on the principles of integrity and loyalty. We pride ourselves in our world-class energy solutions and the success we achieve for our clients.
+                        By combining our expert energy engineers with the latest technologies and concepts for building optimization, our mission is to build the energy efficiency market through the continuous improvement of existing facilities and the creation of efficient, sustainable, new buildings.
+                        Our objective is to drive and develop the energy efficiency market in Asia Pacific. In a region experiencing soaring energy prices due to drastic cuts to energy subsidies and increasing energy demand, we strive to create solutions for our clients that help them remain competitive.
+                        We improve existing buildings by performing highly detailed Energy Audits that provide effective savings opportunities. Synergy Efficiency Solutions assists companies in the implementation of savings solutions by minimizing investment costs through our ESCO financing options.
+                    </p>
                   </div>
                 </div>
 
