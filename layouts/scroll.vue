331c7e58dee1f2ccf5fa46846100aba70979df8c
Very Dev 01 <giideonsiregar@gmail.com>
2019-03-15 17:25:12 +0700

    Test Scroll

---
+++ layouts/scroll.vue
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+  data: () => ({
+    offsetTop: 0
+  }),
+
+  methods: {
+    onScroll (e) {
+      this.offsetTop = e.target.scrollTop
+    }
+  }
+})
--- pages/page.vue
+++ pages/page.vue
@@ -1,26 +1,40 @@
 <template>
-  <v-container fluid>
-    <v-layout>
-      <section id="page" class="about">
-        <h1 class="page-title">Static Page SES</h1>
-        <br><hr><br>
-        <div class="content">
-          <p>Synergy Efficiency Solutions is a company founded on the principles of integrity and loyalty. We pride ourselves in our world-class energy solutions and the success we achieve for our clients.</p>
-          <p>By combining our expert energy engineers with the latest technologies and concepts for building optimization, our mission is to build the energy efficiency market through the continuous improvement of existing facilities and the creation of efficient, sustainable, new buildings.</p>
-          <p>Our objective is to drive and develop the energy efficiency market in Asia Pacific. In a region experiencing soaring energy prices due to drastic cuts to energy subsidies and increasing energy demand, we strive to create solutions for our clients that help them remain competitive.</p>
-          <p>We improve existing buildings by performing highly detailed Energy Audits that provide effective savings opportunities. Synergy Efficiency Solutions assists companies in the implementation of savings solutions by minimizing investment costs through our ESCO financing options.</p>
-        </div>
-      </section>
-    </v-layout>
-</v-container>
+  <div id="app">
+    <v-app id="inspire">
+      <div>
+        <v-layout column justify-center align-center>
+          <v-subheader>Offset Top</v-subheader>
+          {{ offsetTop }}
+        </v-layout>
+        <v-container
+          id="scroll-target"
+          style="max-height: 400px"
+          class="scroll-y"
+        >
+          <v-layout
+            v-scroll:#scroll-target="onScroll"
+            column
+            align-center
+            justify-center
+            style="height: 1000px"
+          >
+          </v-layout>
+        </v-container>
+      </div>
+    </v-app>
+  </div>
 </template>
-<style scoped>
-main.v-content {
-    padding-top: 80px !important;
-}
-</style>
+
 <script>
-export default {
-  layout: 'normal',
+  export default {
+    data: () => ({
+      offsetTop: 0
+    }),
+
+    methods: {
+      onScroll (e) {
+        this.offsetTop = e.target.scrollTop
+      }
+    }
   }
 </script>
