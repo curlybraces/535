9ea0805fe4cf29cc839c7f5889735f9fc1d3b38c
pryspry <halo@pryspry.com>
2019-03-19 20:19:49 +0700

    Improve scroller

---
+++ components/OurServices.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs6 class="ses_serviceslist">
+      <v-dialog v-model="dialog" width="600px">
+
+      <template v-slot:activator="{ on }">
+        <v-card v-on="on">
+        <v-card-text class="px-0"></v-card-text>
+          <v-img src="/img/EnergyPerformanceSimulation.png"
+          max-height="72"
+          contain
+          />
+            <v-card-text class="px-0">Building Performance Simulation</v-card-text>
+            </v-card>
+            </template>
+
+            <v-card>
+              <v-card-title>
+                <span class="headline">Building Performance Simulation</span>
+              </v-card-title>
+              <v-card-text>Synergy Efficiency Solutions helps develop and design energy efficient green buildings.
+              We work with key stakeholders to identify a variety of structural changes that directly impact energy efficiency. Our Energy Efficiency Experts compile a detailed report demonstrating key opportunities for energy efficiency improvement and cost savings. We then help manage your energy, saving money short-term and long-term.
+              Integrated Design Services
+              Our involvement in any sustainable, green building design project starts by setting performance targets that we then help guide to achieve. Our collaboration and involvement with architects, engineers, project managers and other integral parties is critical to success as we build energy savings solutions. There is constant assessment and reassessment as we strive for the optimal solutions.
+              Conceptualization Phase
+              This early stage of the design process focuses on the conceptual basis for the whole design process. This is where all the pre-design analyses are carried out. It is characterized as the definition of the design problem and criteria. This phase can be the most pivotal period to cost effectively reduce energy use in buildings. Our Energy Efficiency Experts assist owners and project teams in a variety of ways before major design efforts take place. This can include original research to support conceptual design ideas or product and service development. At this stage, our team utilizes four areas to guide the process and understand building performance: Pre-Design Climate Analysis, Pre-Design Benchmarking and Goal Setting, Energy "Brain-Storming" and Criteria Definition. In the Conceptualization phase, the focus is understanding and defining how to achieve a sustainable, green building and benefit from long-term energy savings.
+              Criteria Design Phase
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+
+      </v-flex>
+      <v-flex xs6 class="ses_serviceslist">
+      <v-card>
+      <v-card-text class="px-0"></v-card-text>
+        <v-img src="/img/EnergyAudits.png"
+        max-height="72"
+        contain
+        />
+          <v-card-text class="px-0">Energy Audits</v-card-text>
+          </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs6  class="ses_serviceslist">
+      <v-card>
+      <v-card-text class="px-0"></v-card-text>
+        <v-img src="/img/LEEDCertification.png"
+        max-height="72"
+        contain
+        />
+          <v-card-text class="px-0">Green Building Certification</v-card-text>
+          </v-card>
+      </v-flex>
+      <v-flex xs6  class="ses_serviceslist">
+      <v-card>
+      <v-card-text class="px-0"></v-card-text>
+        <v-img src="/img/ESCOFinancing.png"
+        max-height="72"
+        contain
+        />
+          <v-card-text class="px-0">ESCO Financing</v-card-text>
+          </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<style>
+.ses_serviceslist {
+  }
+
+</style>
--- dist/sw.js
+++ dist/sw.js
@@ -2,80 +2,80 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/098152b08870ba7329b6.js",
-    "revision": "94b1ff322bbdc49c40e8f199d815e8ce"
+    "url": "/_nuxt/09e3c8ef9fa0c4264e26.js",
+    "revision": "5af9e2ac2320e5554862e54fbf028b5a"
   },
   {
-    "url": "/_nuxt/12840c29638451df9a1b.js",
-    "revision": "a01caa713c713b6380342914f1922dae"
+    "url": "/_nuxt/2916ce84ae2bcf0befcd.js",
+    "revision": "4cff3d91348dff3db7709732de3188ac"
   },
   {
-    "url": "/_nuxt/14c0dae7c1f759fa954d.js",
-    "revision": "bd073daff1d8a77256216f2e2c20ea2e"
+    "url": "/_nuxt/2e934d1bcfb77e394248.js",
+    "revision": "9443002c5ba73522f1f2615af17b4a23"
   },
   {
-    "url": "/_nuxt/1fc463e08972bfad4df9.js",
-    "revision": "7ea05a7c088bed33dc5172a801776e60"
+    "url": "/_nuxt/35aaec3c13dc855317da.js",
+    "revision": "a0fef6219d02ccbce7c5b190daca5746"
   },
   {
-    "url": "/_nuxt/22eff94449de5f2f9d57.js",
-    "revision": "678d0f24d5e00ccf792eff544f3e30af"
+    "url": "/_nuxt/438e14af5f339d9789cd.js",
+    "revision": "aecaf7f9f1dae5be9523da77e2bd0358"
   },
   {
-    "url": "/_nuxt/3461b4407ae8807a324b.js",
-    "revision": "bdef855503c2de6979bae3fcb70c6546"
+    "url": "/_nuxt/44a589f9d4f8fba803d7.js",
+    "revision": "c4504cbc57d8f6f4b0a793f47adc6362"
   },
   {
-    "url": "/_nuxt/3c981499c7fb82feb0a2.js",
-    "revision": "8ae51cdb60ff4c6a24a5ec3c287a7e1a"
+    "url": "/_nuxt/52ce493f690f396785b6.js",
+    "revision": "464263369837d349fe6993f5ac8f5b6c"
   },
   {
-    "url": "/_nuxt/637689f7238a1dd22486.js",
-    "revision": "d96587e1cd5610d4e074fe288f3e6404"
+    "url": "/_nuxt/5a3e226d3cb5c9499934.js",
+    "revision": "4bc8292352fad77b7eecc317dd4c6d04"
   },
   {
-    "url": "/_nuxt/77cf5ede085d87302f6b.js",
-    "revision": "578408f351c4d2c56fd600c453ec32e4"
+    "url": "/_nuxt/61d83b7c367f319e0e5e.js",
+    "revision": "9281b00049eb0ece26f2fd3591ecb45a"
   },
   {
-    "url": "/_nuxt/8a5344678abda40a3ea5.js",
-    "revision": "2aab30d0eedd95a64619593b73bfb39f"
+    "url": "/_nuxt/6fd1b2ccc51c2c9912ed.js",
+    "revision": "8f120306a0a118f9d2b3cfd3823609d4"
   },
   {
-    "url": "/_nuxt/8fc0baedacac064f904c.js",
-    "revision": "12b79a054d79373968758252c7f0f895"
+    "url": "/_nuxt/71fdc3f802bf5d0b4eaa.js",
+    "revision": "f5a4d99cf6bd358d1ce2c9bc539e24fe"
   },
   {
-    "url": "/_nuxt/92a4e128a400080f68dd.js",
-    "revision": "c60dd741cbf509219b7eaabe9325a08b"
+    "url": "/_nuxt/745f2bea808324c551d6.js",
+    "revision": "69e3d35983a1303ccd20874e0e009dc6"
   },
   {
-    "url": "/_nuxt/953994ec26f91bee6bd8.js",
-    "revision": "ebd1dd89d95e8b10eb63afd3bc4ce52e"
+    "url": "/_nuxt/7734b8e3684a907de591.js",
+    "revision": "42ba812fb21eec5d9e36232da8f79218"
+  },
+  {
+    "url": "/_nuxt/866bf74df9d07bd34967.js",
+    "revision": "c35097d2331107625339a4f22e150f0e"
+  },
+  {
+    "url": "/_nuxt/917b4aaa51f79c6b7c6c.js",
+    "revision": "9e732351f1be5b6b64deacf0dc8b3dc5"
+  },
+  {
+    "url": "/_nuxt/a454f4a103324624b9cb.js",
+    "revision": "577a17c1a9bad55999ac67e356b1d49a"
   },
   {
     "url": "/_nuxt/a6f9b2f3b157c4fb4d98.js",
     "revision": "0655de020c1bae65a4520e63af684c89"
   },
   {
-    "url": "/_nuxt/a75456b9b4ae95104aa5.js",
-    "revision": "37d24378da4df32b1407c6c3a9120d5c"
+    "url": "/_nuxt/c22e3875cf54588a1f7c.js",
+    "revision": "0e46273489750c0263ea66b76b10568b"
   },
   {
-    "url": "/_nuxt/c8b83c54a690ab322fa7.js",
-    "revision": "25933a585df129181de69b826894ed98"
-  },
-  {
-    "url": "/_nuxt/d738c0ab14df2ef75bfe.js",
-    "revision": "f977eb8f8090e7ca9f4b7b3861d79d83"
-  },
-  {
-    "url": "/_nuxt/e8228725a09366d73dd4.js",
-    "revision": "9f926feb579e4c2e3fed1f1f3ce9ec15"
-  },
-  {
-    "url": "/_nuxt/fe8e5cfd5ddaf9b8fca7.js",
-    "revision": "70750c2d93af7dad9f4c62cd643ba2c0"
+    "url": "/_nuxt/fc45c32411f07af721a9.js",
+    "revision": "d345e5431a34ad98419c928a2f16b7b8"
   }
 ], {
   "cacheId": "ses-dev-mode",
