684d406c45d40cf5573aaf6a5b2d3b94a7d8282f
pryspry <halo@pryspry.com>
2019-03-19 13:55:30 +0700

    Add scrollable container

--- pages/index.vue
+++ pages/index.vue
@@ -30,22 +30,32 @@
             <h2 class="ses_subheadline">More</h2>
           </v-container>
         </div>
-        <div class="slide ses_coverpage text-xs-center">
-          <v-container>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus nisi in dolor fermentum consectetur. Proin lobortis, ligula ut vehicula tincidunt, nibh sem ultricies velit, laoreet hendrerit magna augue nec ex. Pellentesque
-              cursus consequat porta. Fusce et nisi ornare, euismod urna et, ultrices mauris. Etiam aliquet nisi diam, sed sollicitudin nunc facilisis sit amet. Aliquam erat volutpat. Proin ac volutpat erat. Nullam fermentum pellentesque auctor.
-              Etiam ac ex quis dolor molestie blandit. Cras tincidunt tortor a libero congue, eu pretium tellus gravida. Nulla facilisi. Nunc tempor commodo sem, non ultrices tellus aliquet eu. Nam vulputate quam nec est suscipit ornare. Mauris sit
-              amet justo felis.</p>
-            <p>Nam sed accumsan nulla, eget iaculis risus. Pellentesque maximus diam sit amet vulputate bibendum. Pellentesque justo metus, sollicitudin id laoreet nec, auctor a urna. Nulla risus augue, porta tempus placerat non, rutrum sed erat. Nam
-              luctus eros quis nulla dignissim, et molestie massa fermentum. Cras aliquam lectus a dapibus ornare. Etiam viverra luctus iaculis.</p>
-            <p>Nulla semper nec elit id ullamcorper. Donec finibus leo eget eros interdum, sed tincidunt lorem sagittis. Mauris a pharetra orci. Mauris arcu tellus, sagittis quis sodales at, auctor in nunc. Sed tristique dapibus magna, nec
-              ullamcorper nibh condimentum vel. Phasellus pellentesque interdum velit accumsan ullamcorper. In ullamcorper, diam vitae convallis sodales, ex augue egestas odio, in posuere ligula velit sed turpis.</p>
-            <p>Etiam commodo a diam a congue. Sed ullamcorper tortor rhoncus, congue urna a, dapibus mi. Donec in arcu tincidunt, sagittis lectus non, pellentesque ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-              himenaeos. Proin at nisi vitae urna elementum pretium. Vestibulum a mattis ipsum, scelerisque pharetra nunc. Etiam convallis quis neque ac auctor. Aliquam pulvinar velit efficitur tempus mattis. Phasellus luctus ac augue nec elementum.
-              Aliquam mattis quam non aliquet egestas. Suspendisse potenti. Sed eu dui ut tortor volutpat placerat mattis vel ligula.</p>
-            <p>Donec sollicitudin dolor sapien, sit amet viverra lectus laoreet ac. In auctor sodales sem sit amet sagittis. Nullam quis metus diam. Nam vitae pretium ante. Aliquam interdum justo sed vehicula malesuada. Morbi justo lacus, vulputate
-              nec dignissim sit amet, posuere nec arcu. Suspendisse potenti. Mauris pellentesque elementum dapibus. Integer ut nisl volutpat ante blandit vestibulum et ac sem. Etiam elementum posuere urna, at euismod massa dapibus interdum. Phasellus
-              tincidunt eget urna ut bibendum. Cras in libero at sapien mattis gravida. Proin id euismod tortor. Donec id lacus vel nibh iaculis consequat in vel lorem. Integer mi velit, tempus et nulla eget, suscipit interdum urna.</p>
+        <div class="slide ses_coverpage">
+          <v-layout column justify-center align-center>
+            <img src="/img/favicon.png" width="100px" />
+            <h1>About SES</h1>
+          </v-layout>
+          <v-container id="scroll-target" style="max-height: 400px" class="scroll-y">
+            <v-layout v-scroll:#scroll-target="onScroll" column align-center justify-center style="height: 1000px" pa-3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus nisi in dolor fermentum consectetur. Proin lobortis, ligula ut vehicula tincidunt, nibh sem ultricies velit, laoreet hendrerit magna augue nec ex. Pellentesque
+                cursus consequat porta. Fusce et nisi ornare, euismod urna et, ultrices mauris. Etiam aliquet nisi diam, sed sollicitudin nunc facilisis sit amet. Aliquam erat volutpat. Proin ac volutpat erat. Nullam fermentum pellentesque auctor.
+                Etiam ac ex quis dolor molestie blandit. Cras tincidunt tortor a libero congue, eu pretium tellus gravida. Nulla facilisi. Nunc tempor commodo sem, non ultrices tellus aliquet eu. Nam vulputate quam nec est suscipit ornare. Mauris sit
+                amet justo felis.</p>
+              <p>Nam sed accumsan nulla, eget iaculis risus. Pellentesque maximus diam sit amet vulputate bibendum. Pellentesque justo metus, sollicitudin id laoreet nec, auctor a urna. Nulla risus augue, porta tempus placerat non, rutrum sed erat.
+                Nam
+                luctus eros quis nulla dignissim, et molestie massa fermentum. Cras aliquam lectus a dapibus ornare. Etiam viverra luctus iaculis.</p>
+              <p>Nulla semper nec elit id ullamcorper. Donec finibus leo eget eros interdum, sed tincidunt lorem sagittis. Mauris a pharetra orci. Mauris arcu tellus, sagittis quis sodales at, auctor in nunc. Sed tristique dapibus magna, nec
+                ullamcorper nibh condimentum vel. Phasellus pellentesque interdum velit accumsan ullamcorper. In ullamcorper, diam vitae convallis sodales, ex augue egestas odio, in posuere ligula velit sed turpis.</p>
+              <p>Etiam commodo a diam a congue. Sed ullamcorper tortor rhoncus, congue urna a, dapibus mi. Donec in arcu tincidunt, sagittis lectus non, pellentesque ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos
+                himenaeos. Proin at nisi vitae urna elementum pretium. Vestibulum a mattis ipsum, scelerisque pharetra nunc. Etiam convallis quis neque ac auctor. Aliquam pulvinar velit efficitur tempus mattis. Phasellus luctus ac augue nec
+                elementum.
+                Aliquam mattis quam non aliquet egestas. Suspendisse potenti. Sed eu dui ut tortor volutpat placerat mattis vel ligula.</p>
+              <p>Donec sollicitudin dolor sapien, sit amet viverra lectus laoreet ac. In auctor sodales sem sit amet sagittis. Nullam quis metus diam. Nam vitae pretium ante. Aliquam interdum justo sed vehicula malesuada. Morbi justo lacus, vulputate
+                nec dignissim sit amet, posuere nec arcu. Suspendisse potenti. Mauris pellentesque elementum dapibus. Integer ut nisl volutpat ante blandit vestibulum et ac sem. Etiam elementum posuere urna, at euismod massa dapibus interdum.
+                Phasellus
+                tincidunt eget urna ut bibendum. Cras in libero at sapien mattis gravida. Proin id euismod tortor. Donec id lacus vel nibh iaculis consequat in vel lorem. Integer mi velit, tempus et nulla eget, suscipit interdum urna.</p>
+            </v-layout>
           </v-container>
         </div>
       </div>
@@ -59,22 +69,32 @@
           </v-container>
         </div>
         <div class="slide ses_coverpage text-xs-center">
-          <v-container>
+        <v-layout column justify-center align-center>
+          <img src="/img/favicon.png" width="100px" />
+          <h1>About SES</h1>
+        </v-layout>
+        <v-container id="scroll-target" style="max-height: 400px" class="scroll-y">
+          <v-layout v-scroll:#scroll-target="onScroll" column align-center justify-center style="height: 1000px" pa-3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus nisi in dolor fermentum consectetur. Proin lobortis, ligula ut vehicula tincidunt, nibh sem ultricies velit, laoreet hendrerit magna augue nec ex. Pellentesque
               cursus consequat porta. Fusce et nisi ornare, euismod urna et, ultrices mauris. Etiam aliquet nisi diam, sed sollicitudin nunc facilisis sit amet. Aliquam erat volutpat. Proin ac volutpat erat. Nullam fermentum pellentesque auctor.
               Etiam ac ex quis dolor molestie blandit. Cras tincidunt tortor a libero congue, eu pretium tellus gravida. Nulla facilisi. Nunc tempor commodo sem, non ultrices tellus aliquet eu. Nam vulputate quam nec est suscipit ornare. Mauris sit
               amet justo felis.</p>
-            <p>Nam sed accumsan nulla, eget iaculis risus. Pellentesque maximus diam sit amet vulputate bibendum. Pellentesque justo metus, sollicitudin id laoreet nec, auctor a urna. Nulla risus augue, porta tempus placerat non, rutrum sed erat. Nam
+            <p>Nam sed accumsan nulla, eget iaculis risus. Pellentesque maximus diam sit amet vulputate bibendum. Pellentesque justo metus, sollicitudin id laoreet nec, auctor a urna. Nulla risus augue, porta tempus placerat non, rutrum sed erat.
+              Nam
               luctus eros quis nulla dignissim, et molestie massa fermentum. Cras aliquam lectus a dapibus ornare. Etiam viverra luctus iaculis.</p>
             <p>Nulla semper nec elit id ullamcorper. Donec finibus leo eget eros interdum, sed tincidunt lorem sagittis. Mauris a pharetra orci. Mauris arcu tellus, sagittis quis sodales at, auctor in nunc. Sed tristique dapibus magna, nec
               ullamcorper nibh condimentum vel. Phasellus pellentesque interdum velit accumsan ullamcorper. In ullamcorper, diam vitae convallis sodales, ex augue egestas odio, in posuere ligula velit sed turpis.</p>
-            <p>Etiam commodo a diam a congue. Sed ullamcorper tortor rhoncus, congue urna a, dapibus mi. Donec in arcu tincidunt, sagittis lectus non, pellentesque ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-              himenaeos. Proin at nisi vitae urna elementum pretium. Vestibulum a mattis ipsum, scelerisque pharetra nunc. Etiam convallis quis neque ac auctor. Aliquam pulvinar velit efficitur tempus mattis. Phasellus luctus ac augue nec elementum.
+            <p>Etiam commodo a diam a congue. Sed ullamcorper tortor rhoncus, congue urna a, dapibus mi. Donec in arcu tincidunt, sagittis lectus non, pellentesque ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+              inceptos
+              himenaeos. Proin at nisi vitae urna elementum pretium. Vestibulum a mattis ipsum, scelerisque pharetra nunc. Etiam convallis quis neque ac auctor. Aliquam pulvinar velit efficitur tempus mattis. Phasellus luctus ac augue nec
+              elementum.
               Aliquam mattis quam non aliquet egestas. Suspendisse potenti. Sed eu dui ut tortor volutpat placerat mattis vel ligula.</p>
             <p>Donec sollicitudin dolor sapien, sit amet viverra lectus laoreet ac. In auctor sodales sem sit amet sagittis. Nullam quis metus diam. Nam vitae pretium ante. Aliquam interdum justo sed vehicula malesuada. Morbi justo lacus, vulputate
-              nec dignissim sit amet, posuere nec arcu. Suspendisse potenti. Mauris pellentesque elementum dapibus. Integer ut nisl volutpat ante blandit vestibulum et ac sem. Etiam elementum posuere urna, at euismod massa dapibus interdum. Phasellus
+              nec dignissim sit amet, posuere nec arcu. Suspendisse potenti. Mauris pellentesque elementum dapibus. Integer ut nisl volutpat ante blandit vestibulum et ac sem. Etiam elementum posuere urna, at euismod massa dapibus interdum.
+              Phasellus
               tincidunt eget urna ut bibendum. Cras in libero at sapien mattis gravida. Proin id euismod tortor. Donec id lacus vel nibh iaculis consequat in vel lorem. Integer mi velit, tempus et nulla eget, suscipit interdum urna.</p>
-          </v-container>
+          </v-layout>
+        </v-container>
         </div>
       </div>
 
@@ -229,6 +249,15 @@ export default {
   layout: 'front',
   components: {
     Particles
+  },
+  data: () => ({
+    offsetTop: 0
+  }),
+
+  methods: {
+    onScroll(e) {
+      this.offsetTop = e.target.scrollTop
+    }
   }
 }
 </script>
@@ -318,15 +347,11 @@ export default {
   background-repeat: repeat;
 }
 
-
-
 #ses_more {
   color: #eeeeee;
 }
 
-.about #ses_more,
-.ses_splash #ses_more,
-.ses_intro #ses_more {
+.ses_splash #ses_more {
   position: absolute;
   top: 85%;
   margin: 0 auto 0;
@@ -338,58 +363,15 @@ export default {
   letter-spacing: 2px;
 }
 
-.ses_middleone #ses_more {
-  position: absolute;
-  bottom: 0;
-  top: 90%;
-  width: 33%;
-  margin: 0 auto 0;
-  text-align: center;
-  text-decoration: none;
-  font-weight: 600;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-}
-
-.ses_middleone #ses_moretop {
-  position: absolute;
-  bottom: 0;
-  top: 15%;
-  width: 30%;
-  margin: 0 auto 0;
-  text-align: center;
-  text-decoration: none;
-  font-weight: 600;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-}
-
-.ses_middleone .ses_headline {
-  font-size: 20px;
-  padding-top: 20px;
-  padding-bottom: 10px;
-  width: 80%;
-  margin: 0 auto 0;
-}
-
-.about #ses_more,
-.ses_middleone #ses_more .ses_headline-more,
-.ses_intro #ses_more .ses_headline-more {
-  color: #222;
-}
 
 .fp-controlArrow.fp-prev,
 .fp-controlArrow.fp-next {
   display: none;
 }
 
-.about {
-  background: #fff;
-}
-
 
 #ses_particles {
-  z-index: 9;
+  z-index: 1;
   position: absolute;
   width: 100%;
   height: auto;
