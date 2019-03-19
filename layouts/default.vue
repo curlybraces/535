b0c943835b622647ded83e376e39d21fbff1e57d
pryspry <halo@pryspry.com>
2019-03-19 10:17:31 +0700

    Page improvement

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,45 +1,43 @@
 <template>
-  <v-app>
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
-      <div class="ses-logo-push">
-        <nuxt-link to="/"><img src="/img/logosesfull.png" /></nuxt-link>
-      </div>
-      <MenuItems />
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-    <div class="ses-logo"><nuxt-link to="/"><img src="/img/logoses.png" /></nuxt-link></div>
+<v-app>
+  <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+    <div class="ses-logo-push">
+      <nuxt-link to="/"><img src="/img/logosesfull.png" /></nuxt-link>
+    </div>
+    <MenuItems />
+  </v-navigation-drawer>
+  <v-toolbar app :clipped-left="clipped">
+    <div class="ses-logo">
+      <nuxt-link to="/"><img src="/img/logoses.png" /></nuxt-link>
+    </div>
     <v-spacer></v-spacer>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-    </v-toolbar>      <v-list>
-            <v-list-tile v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-                <v-list-tile-content>
-                    <v-list-tile-title v-text="item.title"/>
-                </v-list-tile-content>
-            </v-list-tile>
-        </v-list>
-      <v-content>
-        <nuxt/>
-      </v-content>
-      </v-app>
+    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+  </v-toolbar>
+
+  <v-content>
+    <nuxt />
+  </v-content>
+</v-app>
 </template>
 <script>
-export default {
-    data() {
-        return {
-            clipped: false,
-            drawer: false,
-            fixed: false,
-            miniVariant: false,
-            right: true,
-            rightDrawer: null,
-            title: 'Synergy Efficiency Solutions'
-        }
-    }
-}
+import MenuItems from '~/components/MenuItems.vue'
 
+export default {
+  components: {
+    MenuItems
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: null,
+      title: 'Synergy Efficiency Solutions'
+    }
+  }
+}
 </script>
 <style>
 
